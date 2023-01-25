9c518dd0116764d755a3cb717e71a01d9a654ff2
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-01-25 00:10:51 -0500

    Fix(OpenAi): Clean GPT-3 response
    - Clean response text by removing 'Output:' and 'Response:' prefixes
    - Add fs module to source/openai.ts
    - Update temperature to 0.35

--- source/openai.ts
+++ source/openai.ts
@@ -1,25 +1,21 @@
 import chalk from 'chalk';
 import {type OpenAIApi, Configuration as _1} from 'openai';
+import fs from 'node:fs';
 
 import { promtps } from './prompts';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-const generateRandTemp = () => Math.random() * 0.4 + 0.5;
-generateRandTemp();
 
-function randomIntFromInterval(min: number, max: number) { // min and max included 
-	return Math.floor(Math.random() * (max - min + 1) + min)
+const cleanGpt3Response = (response: string) => {
+	if (response.startsWith('Output:')) {
+		return response.substring('Output:'.length);
+	}
+	if (response.startsWith('Response:')) {
+		return response.substring('Response:'.length);
+	}
+	return response;
 }
-
-
-// The commit messages should be in the imperative mood, and should be in the form of a single line.
-// The commit message should provide enough context for the reader to understand the changes. Describe why a change is being made. Do not simply describe the change. For example, if you are fixing a bug, the commit message should explain what the bug was, and how you fixed it. If you are adding a new feature, the commit message should explain what the feature is, and why it is being added.
-// How does it address the issue? What is the motivation for the change? What is the rationale behind the implementation? What alternatives did you consider? What effects does the patch have? Why is this the best implementation? The commit message should not exceed 50 characters. 
-
-
-// Summarize the changes made in the given \`git diff\` output in a clear and concise commit message that accurately reflects the modifications made to the code-base. Use best practices for writing commit messages, and be sure to follow the conventional commit format.
-
 
 interface ICommitMessage {
 	id: number;
@@ -49,8 +45,7 @@ const generateCommitMessages = async ({
 		throw new Error('No diff provided');
 	}
 
-	const selectedPrompt =  promtps[randomIntFromInterval(0, promtps.length - 2)];//promtps[3];// 
-	console.log(chalk.blue(`Selected prompt: ${selectedPrompt?.id}`));
+	const selectedPrompt =  promtps[3];
 
 	const prompt = selectedPrompt?.prompt({
 		diff,
@@ -63,10 +58,8 @@ const generateCommitMessages = async ({
 			model,
 			prompt,
 			max_tokens,
-			temperature: 0.25,
+			temperature: 0.35,
 		});
-
-		console.log(response.data.choices[0]?.text);
 
 		if (response.data.choices.length === 0 || !response.data.choices[0]?.text) {
 			console.log(chalk`(${maxRetries - currentRetry + 1}/${maxRetries}) {red Attempting to generate commit messages again...}`);
@@ -75,9 +68,12 @@ const generateCommitMessages = async ({
 		}
 
 		try {
-			const commitMessagesJson: {commit_messages: ICommitMessage[]} = JSON.parse(response.data.choices[0].text);
+			const gptResponse = cleanGpt3Response(response.data.choices[0].text);
+			const commitMessagesJson: {commit_messages: ICommitMessage[]} = JSON.parse(gptResponse);
 			return commitMessagesJson.commit_messages;
 		} catch (e) {
+			fs.appendFileSync('./error.log', "\n\n\n");
+			fs.appendFileSync('./error.log', response.data.choices[0].text);
 			console.log(chalk`(${maxRetries - currentRetry + 1}/${maxRetries}) {red Attempting to generate commit messages again...}`);
 			await sleep(retryDelay);
 		}
